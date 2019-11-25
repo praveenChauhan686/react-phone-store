17e9621034164cc2da6e7f7f63b9a88e51dbca9c
praveenChauhan686 <51928782+praveenChauhan686@users.noreply.github.com>
2019-11-25 18:14:42 +0530

    Add files via upload

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,45 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import Title from "../Title";
+import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
 import { ProductConsumer } from "../../Context";
-import PayPal from "./PayPal";
-
-export default function CartTotals({ value, history }) {
-  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
-
-  return (
-    <React.Fragment>
-      <div className="container">
-        <div className="row">
-          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
-            <Link to="/">
-              <button
-                className="btn btn-outline-danger text-uppercase mb-3 px-5"
-                type="button"
-                onClick={() => clearCart()}
-              >
-                clear cart
-              </button>
-            </Link>
-            <h5>
-              <span className="text-title">subtotal:</span>
-              <strong>$ {cartSubTotal}</strong>
-            </h5>
-            <h5>
-              <span className="text-title">tax:</span>
-              <strong>$ {cartTax}</strong>
-            </h5>
-            <h5>
-              <span className="text-title">total:</span>
-              <strong>$ {cartTotal}</strong>
-            </h5>
-            <PayPal
-              total={cartTotal}
-              clearCart={clearCart}
-              history={history}
-            ></PayPal>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+import CartList from "./CartList";
+import CartTotals from "./CartTotals";
+export default class Cart extends Component {
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart"></Title>
+                  <CartColumns></CartColumns>
+                  <CartList value={value}></CartList>
+                  <CartTotals value={value} history={this.props.history}>
+                    {" "}
+                  </CartTotals>
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart></EmptyCart>;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
+  }
 }
